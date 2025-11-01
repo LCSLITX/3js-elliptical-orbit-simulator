c9bfab080987a7f7537fec3671c96fc83fbcb4c2
LCSLITX <lucassauro.lt@gmail.com>
2025-11-01 09:24:38 -0300

    fix vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,5 @@ export default {
   // If `base` is not configured properly then the necessary files won't be found by GH-Pages.
   // Change the repository name as the example shows.
   // base: process.env.NODE_ENV === 'production' ? '/<REPOSITORY_NAME>/' : ''
-  base: process.env.NODE_ENV === 'production' ? '/super-duper-sphere/' : '',
+  base: process.env.NODE_ENV === 'production' ? '/3js-elliptical-orbit-simulator/' : '',
 }
